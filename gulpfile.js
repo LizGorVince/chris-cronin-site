ecb492642e01bf3da41338f19fd579cedb5da78b
Liz Verde <liz@goramandvincent.com>
2018-12-28 12:06:23 +0000

    trying to sort out gulp but failed, css changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,58 @@
 'use strict';
 
 var gulp = require('gulp'),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    svgmin = require('gulp-svgmin'),
+    svgstore = require('gulp-svgstore'),
+    path = require('path'),
+    rename = require('gulp-rename'),
+    // autoprefix = require('gulp-autoprefixer'),
+    // cssnano = require('gulp-cssnano'),
+    // concat = require('gulp-concat'),
+    // uglify = require('gulp-uglify'),
+
+    // modernizr = require('gulp-modernizr'),
+    // browserSync = require('browser-sync').create();
+    series = require('gulp-series'),
+    parallel = require('gulp-ccr-parallel'),
+    watch = require('gulp-watch');
 
 gulp.task('sass', function() {
-  return gulp.src('assets/scss')
+  return gulp.src('assets/scss/main.scss')
     .pipe(sass())
     .pipe(gulp.dest('assets/css'))
 });
 
-//sass --watch input:output
+gulp.task('watch', function() {
+  gulp.watch('assets/scss/**/*.scss', ['sass'])
+});
+
+gulp.task('svgs', function () {
+  return gulp
+    .src('assets/img/svg/*.svg')
+    .pipe(rename({prefix: 'shape-'}))
+    .pipe(svgmin(function (file) {
+      var prefix = path.basename(file.
+        relative, path.extname(file.
+        relative));
+      return {
+        plugins: [{
+          cleanupIDs: {
+            prefix: prefix + '-',
+            minify: true
+          }
+        }]
+      }
+    }))
+    .pipe(svgstore())
+    .pipe(rename('svg-defs.svg'))
+    .pipe(gulp.dest('views/utility'));
+});
+
+//Watch task
+gulp.task('default',
+  gulp.series('clean', gulp.parallel('scripts', 'styles'),
+  function() {
+    ...
+  }
+));
